a0353ad3f11f2da5bea826fd8b2ab664196bb7aa
PradoCarretero <p.carreteroayala@gmail.com>
2022-10-06 12:39:56 +0200

    add feedback trycatch delete and post api data

--- prueba-tecnica-prado/front/src/services/apiRequests.js
+++ prueba-tecnica-prado/front/src/services/apiRequests.js
@@ -10,20 +10,28 @@ const getApiData = async () => {
   }
 };
 
-const postApiData = (data) => {
-  return fetch(`${URL}/users`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => response.json())
-    .catch((error) => error.log(`Error fetch post: ${error}`));
+const postApiData = async (data) => {
+  try {
+    const resp = await fetch(`${URL}/users`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+    const result = await resp.json();
+    return result;
+  } catch (error) {
+    error.log(`Error fetch post: ${error}`);
+  }
 };
 
-const deleteApiData = (id) => {
-  return fetch(`${URL}/user/${id}`, {
-    method: "DELETE",
-  });
+const deleteApiData = async (id) => {
+  try {
+    const response = await fetch(`${URL}/user/${id}`, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    error.log(`Error fetch delete: ${error}`);
+  }
 };
 
 const putApiData = (id, newRow) => {
